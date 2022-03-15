d85913b1f870c5c053646e7969116b1d758d840d
Dan Greco <dan.greco@live.com>
2022-03-15 00:41:47 -0400

    fix formatting

--- src/main.rs
+++ src/main.rs
@@ -10,25 +10,23 @@ use avr_delay::delay_ms;
 use avrd::atmega2560::*;
 
 fn delay(ms: u32) {
-    for _ in 0..(ms / 16) { // idk
-        delay_ms(100)
-    }
+  for _ in 0..(ms / 16) { // idk
+    delay_ms(100)
+  }
 }
 
 #[no_mangle]
-
 pub extern fn main() {
-  
-    let mut out: u8 = 0x00;
-    unsafe { volatile_store(DDRB, 0xff) }
-    loop {
-        out = out ^ 0xff;
-        unsafe { volatile_store(PORTB, out) }
-        delay(1000);
-    }
+  let mut out: u8 = 0x00;
+  unsafe { volatile_store(DDRB, 0xff) }
+   loop {
+    out = out ^ 0xff;
+    unsafe { volatile_store(PORTB, out) }
+    delay(1000);
+  }
 }
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
-    loop {}
+  loop {}
 }
